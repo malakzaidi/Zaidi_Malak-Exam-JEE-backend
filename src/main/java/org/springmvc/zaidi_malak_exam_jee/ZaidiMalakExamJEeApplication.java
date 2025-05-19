73880aa2d6a9a3521a255b14e0aa9efdd479b9a5
Malak Zaidi <malakzaidi815@gmail.com>
2025-05-19 09:09:18 +0000

    change the ArtifactId and groupId to contain the name

---
+++ src/main/java/org/springmvc/zaidi_malak_exam_jee/ZaidiMalakExamJEeApplication.java
@@ -0,0 +1,13 @@
+package org.springmvc.zaidi_malak_exam_jee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaidiMalakExamJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaidiMalakExamJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springmvc/zaidi_malak_exam_jee/ZaidiMalakExamJEeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springmvc.zaidi_malak_exam_jee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaidiMalakExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
